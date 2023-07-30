9adee12c9137049220a23d8839630519df318efa
MaxiSama12 <maxiestebansama12@gmail.com>
2023-07-30 18:46:43 -0300

    creamos la rama Esteban-Home en la que estara todo el front relacionado al Home

--- src/assets/paginas/home/Home.jsx
+++ src/assets/paginas/home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-          <h1>Creando Rama Develop</h1>  
+          <h1>Estructura del Home</h1>
         </div>
     );
 };
