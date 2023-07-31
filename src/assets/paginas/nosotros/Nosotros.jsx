17bbd1db2b37e49e7227661c629aa05d6f144e7c
Yonathan Pachado <jonathanpachado73@gmail.com>
2023-07-31 19:30:51 -0300

    creando rama para nosotros y contacto

---
+++ src/assets/paginas/nosotros/Nosotros.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Nosotros = () => {
+    return (
+        <div>
+            <h1>Estructura de Quienes Somos</h1>
+        </div>
+    );
+};
+
+export default Nosotros;
