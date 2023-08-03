fbf22d600cd921e7ad55f35fc706c41c6269e6f5
MaxiSama12 <maxiestebansama12@gmail.com>
2023-08-03 19:11:02 -0300

    añadimos el front del home y sus estilos

--- src/assets/paginas/home/Home.jsx
+++ src/assets/paginas/home/Home.jsx
@@ -1,10 +1,67 @@
+
+import "./styleHome.css"
 import React from 'react';
 
+
 const Home = () => {
+
+  
     return (
-        <div>
-          <h1>Estructura del Home</h1>
+        <>
+        <main className="">
+            <section className="home row ">
+
+            
+            <article className='card-home col-md-4'>
+              <a href="">
+                <img src="../public/img/meterproducto.png" alt="" />
+              </a>
+                <span>Añadir Producto</span>
+              </article>
+
+              <article className='card-home col-md-4'>
+              <a href="">
+                <img src="../public/img/controlinventario.png" alt="" />
+              </a>
+                <span>Gestion Proveedores</span>
+              </article>
+
+              <article className='card-home col-md-4'>
+              <a href="">
+                <img src="../public/img/almacen.png" alt="" />
+              </a>
+                <span>Almacen</span>
+              </article>
+          
+              <article className='card-home'>
+              <a href="">
+                <img src="../public/img/informes.png" alt="" />
+              </a>
+              <span>Informe y Analisis</span>
+              </article>
+
+              <article className='card-home'>
+              <a href="">
+                <img src="../public/img/gestionpedidos.png" alt="" />
+              </a>
+              <span>Pedidos</span>
+              </article>
+
+              <article className='card-home '>
+              <a href="">
+                <img src="../public/img/controlusuarios.png" alt="" />
+              </a>
+              <span>Control Usuarios</span>
+              </article>
+        
+            </section>
+
+        </main>
+
+        <div className="footer">
+           
         </div>
+        </>
     );
 };
 
